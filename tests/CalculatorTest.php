a90c737ae7df82606eb0b316aabe030d74261dfc
Alec Smrekar <alec.smrekar@gmail.com>
2016-11-18 12:53:56 +0100

    Updated text

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,13 +22,7 @@ class CalculatorTest extends TestCase {
 
   /**
    * TODO
-   * - Create a setUp constructor where you initiate the calculator object
-   * - Test empty input throws an exception
-   * - Test calculator throws an exception when dividing by 0
-   * - Test that if wrong operator is passed to set() the default one is addition
-   * - Make sure the result is displayed with a maximum of 5 decimals
-   * - Create a data provider function that tests different operations and makes sure the result is correct
-   * - Create a test for calculating 10^2 passing a mock AdvancedMathExtension object to the process() function
+   * Tako a look at README.md for some example tasks
    */
 }
 
